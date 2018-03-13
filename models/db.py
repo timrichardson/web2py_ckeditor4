eb164323d223f13971cc1bb90ad0fad5ac1b3a19
Jonathan Clark <jonathan.clark@whatho.net>
2018-03-13 16:30:06 +0000

    Handle lazy_tables = True

--- models/db.py
+++ models/db.py
@@ -11,7 +11,9 @@
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
+    db = DAL('sqlite://storage.sqlite',pool_size=1,
+            check_reserved=['all'],
+            lazy_tables=True)
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
     db = DAL('google:datastore')
